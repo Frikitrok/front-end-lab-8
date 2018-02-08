2437ed7d2622519fd2c31730646e647ede09bc3c
Frikitrok <zgrabovskiy@gmail.com>
2018-02-09 00:45:38 +0200

    add 6 hometask

---
+++ homework_06/js/task1.js
@@ -0,0 +1,12 @@
+var num1 = parseFloat(prompt('Side 1'));
+var num2 = parseFloat(prompt('Side 2'));
+var num3 = parseFloat(prompt('Side 3'));
+
+function findType(num1, num2, num3) {
+
+}
+
+console.log(parseFloat(parseFloat(num1).toFixed(2)));
+console.log(parseFloat(num2).toFixed(2));
+console.log(parseFloat(num3).toFixed(2));
+
