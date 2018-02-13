88cb272b6eeeae31bd5364a21e9a9599a55f428b
Frikitrok <zgrabovskiy@gmail.com>
2018-02-13 16:16:21 +0200

    change prize calculation

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,6 +1,7 @@
 var max = 5;
     attempts = 3;
-    prize = [2, 5, 10];
+    maxPrize = 10;
+    currentPrize = 10;
     win = 0;
 while(true) {
   if(win > 0) {
@@ -9,28 +10,29 @@ while(true) {
     var play = confirm("Do you want to play a game?");
   }
   if(play) {
-    var num = Math.floor(Math.random() * Math.floor(max));
+    var num = Math.floor(Math.random() * Math.floor(max + 1));
     for (var i = 0; i < 3; i++) {
       var answer = prompt("Enter a number from 0 to " + max + "\n"
                           + "Attempsts left: " + attempts + "\n"
                           + "Total prize: " + win + "$" + "\n"
-                          + "Possible prize on current attempt: " + prize[2] + "$");
+                          + "Possible prize on current attempt: " + maxPrize + "$");
       if(num == answer) {
-        win += prize[attempts - 1];
-        console.log("You win " + prize[attempts - 1] + "$");
+        win += currentPrize;
+        console.log("You win " + currentPrize + "$");
         attempts = 3;
         max *= 2;
-        prize.forEach(function(item, index) {
-          prize[index] = item * 3;
-        });
+        maxPrize *= 3;
+        currentPrize = maxPrize;
         break;
       }else {
         attempts--;
+        currentPrize = Math.floor(currentPrize / 2);
         if(attempts==0) {
           console.log('Thank you for a game. Your prize is: ' + win + "$");
           max = 5;
           attempts = 3;
-          prize = [2, 5, 10];
+          maxPrize = 10;
+          currentPrize = maxPrize;
           win = 0;
           break;
         }
