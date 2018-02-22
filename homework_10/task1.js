aa6a34fab63091fc43a0ec9537c8f544a5b0fd13
Frikitrok <zgrabovskiy@gmail.com>
2018-02-22 14:19:18 +0200

    add hometask 10

---
+++ homework_10/task1.js
@@ -0,0 +1,20 @@
+var iterator = 0;
+var timeoutId;
+
+function increaseIteratorBy1() {
+ iterator++;
+ printIteratorValue();
+}
+
+function printIteratorValue() {
+ console.log('Iterator value ', iterator);
+}
+
+function debounce(func, pause) {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(func, pause);
+}
+
+debounce(increaseIteratorBy1, 1000);
+debounce(increaseIteratorBy1, 1000);
+debounce(increaseIteratorBy1, 1000);
